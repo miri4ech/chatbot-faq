25f676dfaff8e5cefe613df3be267b6514514bed
kunisada <miriev56@gmail.com>
2017-08-22 14:04:30 +0900

    feat: layout colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { ThemeProvider } from 'styled-components';
 import ChatBot from './ChatBot.js'
 
 class App extends Component {
   render() {
+    const theme = {
+      background: '#fff',
+      fontFamily: 'Helvetica Neue',
+      headerBgColor: '#14d0d6',
+      headerFontColor: '#fff',
+      botBubbleColor: '#14d0d6',
+      botFontColor: '#fff',
+      userBubbleColor: '#fff',
+      userFontColor: '#4a4a4a',
+    }
+
     return (
       <div>
-        <ChatBot />
+        <ThemeProvider theme={theme}>
+          <ChatBot />
+        </ThemeProvider>
       </div>
     )
   }
